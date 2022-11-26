840a632faa93d4accac57730bf0089af0fc7f155
rai <rai@iMac-de-rai.local>
2022-11-26 09:04:15 +0100

    First commit

---
+++ proyecto/script.js
@@ -0,0 +1,5 @@
+function inventos() {
+    document.getElementById("inventosjs").style.color = "#F0FF00";
+    document.getElementById("inventosjs").innerHTML = "Accede a la sección categorías"; 
+    document.getElementById("inventosjs").style.fontWeight = "900"
+  }
