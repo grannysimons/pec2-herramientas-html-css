c5b9a552deb8fcff89062e14ffc83d19ef76fc62
rai <rai@iMac-de-rai.local>
2022-12-23 00:00:06 +0100

    Subida PEC 2

---
+++ dist/detalle.ed0a183d.js
@@ -0,0 +1,7 @@
+function inventos() {
+    document.getElementById("inventosjs").style.color = "#F0FF00";
+    document.getElementById("inventosjs").innerHTML = "Accede a la secci\xf3n categor\xedas";
+    document.getElementById("inventosjs").style.fontWeight = "900";
+}
+
+//# sourceMappingURL=detalle.ed0a183d.js.map
